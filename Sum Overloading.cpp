74a526d20ca8a29d06c6070f4f5d993ede9500b3
Harshika Kamwal <harshikakamwal@gmail.com>
2025-09-29 19:50:44 +0530

    Add files via upload

---
+++ Sum Overloading.cpp
@@ -0,0 +1,38 @@
+// Harshika Kamwal
+// 25070123508
+
+#include <iostream>
+using namespace std;
+
+    class addition{
+    public:
+
+int sum(int a , int b ){
+        return a+b;
+       
+    }
+int sum(int a , int b , int c){
+        return a+b+c;
+       
+    }
+    
+double sum(double d , double f ){
+        return d+f;
+       
+    }
+   
+};
+int main(){
+
+    addition obj;
+    cout<< obj.sum(10,20)<<endl;
+    cout<< obj.sum(10,20,20)<<endl;
+    cout<< obj.sum(10.8,20.34)<<endl;
+
+    return 0;
+}
+
+// OUTPUT
+// 30
+// 50
+// 31.14
